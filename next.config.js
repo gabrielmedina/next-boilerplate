9e1260f35182078b83693f13e9915eed08e14d7e
Gabriel Medina <gmedina.santos@gmail.com>
2022-03-27 12:46:37 -0400

    feat: use sass to styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    prependData: `@import "variables.scss";`,
+  },
 }
 
 module.exports = nextConfig
